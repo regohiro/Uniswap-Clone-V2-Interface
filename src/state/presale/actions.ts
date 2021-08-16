595aabafb9867420a25f7d8a107d4d28b1376232
REGO350 <riokanehiro228pa@gmail.com>
2021-08-16 23:05:11 +0900

    [remove] unused stuff

--- src/state/presale/actions.ts
+++
@@ -1,4 +0,0 @@
-import { createAction } from '@reduxjs/toolkit';
-
-export const updateBuyLimit = createAction<number>('presale/updateBuyLimit');
-export const updateRemaining = createAction<number>('presale/updateRemaining');
