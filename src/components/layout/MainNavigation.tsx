96447470fa10cb3710e26cfbeea38b97a910d249
REGO350 <riokanehiro228pa@gmail.com>
2021-08-16 23:18:14 +0900

    [update] navigation

--- src/components/layout/MainNavigation.tsx
+++ src/components/layout/MainNavigation.tsx
@@ -1,26 +1,14 @@
-import React from 'react'
-import Link from 'next/link'
+import React from "react";
 import styles from "./Layout.module.css";
+import ConnectButton from "../ConnectButton";
 
 const MainNavigation: React.FC = () => {
   return (
     <header className={styles.header}>
-      <div className={styles.logo}>DontApeThis</div>
-      <nav>
-        <ul>
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/staking">Staking</Link>
-          </li>
-          <li>
-            <Link href="/presale">Presale</Link>
-          </li>
-        </ul>
-      </nav>
+      <h1 className={styles.logo}>Block Swap</h1>
+      <ConnectButton/>
     </header>
-  )
-}
+  );
+};
 
-export default MainNavigation
+export default MainNavigation;
