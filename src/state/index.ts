aa13dc5a605c50c00890738feba00e3ffbe55c97
REGO350 <riokanehiro228pa@gmail.com>
2021-08-20 00:26:15 +0900

    [create] popup state

--- src/state/index.ts
+++ src/state/index.ts
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import thunk from "redux-thunk";
 import user from "./user/reducers";
 import swap from "./swap/reducers";
+import popup from "./popup/reducers";
 
 const store = configureStore({
   reducer: {
     user,
-    swap
+    swap,
+    popup
   },
   middleware: [thunk],
 });
@@ -18,3 +20,4 @@ export type AppDispatch = typeof store.dispatch;
 
 export const selectUser = (state: RootState) => state.user;
 export const selectSwap = (state: RootState) => state.swap;
+export const selectPopup = (state: RootState) => state.popup;
