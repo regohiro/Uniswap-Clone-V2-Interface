8b6fbd126f79ae73df31fda60ff6681552de7802
REGO350 <riokanehiro228pa@gmail.com>
2021-08-19 10:55:16 +0900

    [fix] decimal number not working

--- src/utils/bigNumber.ts
+++ src/utils/bigNumber.ts
@@ -1,13 +1,13 @@
 import { BigNumber, ethers } from 'ethers';
 
 export const toBN = (value: number): BigNumber => {
-  const valueString = value.toLocaleString("fullwide", { useGrouping: false });
+  const valueString = value.toString();
   const valueBN = BigNumber.from(valueString);
   return valueBN;
 }
 
 export const toWei = (value: number, decimals: number = 18): BigNumber => {
-  const valueString = value.toLocaleString("fullwide", { useGrouping: false });
+  const valueString = value.toString();
   const valueWeiBN = ethers.utils.parseUnits(valueString, decimals);
   return valueWeiBN;
 }
@@ -23,3 +23,5 @@ export const fromWei = (valueWeiBN: BigNumber, decimals: number = 18): number =>
   const valueNumber = Number(valueString);
   return valueNumber;
 }
+
+export const tenPow18 = toBN(10).pow(18);
