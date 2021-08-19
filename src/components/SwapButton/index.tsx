f16c5a6c32a1d6cc0d8c52e146a842f185945ab0
REGO350 <riokanehiro228pa@gmail.com>
2021-08-19 21:31:38 +0900

    [create] swap button component

---
+++ src/components/SwapButton/index.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { selectSwap, selectUser } from "../../state";
+import { Button, Spinner } from "react-bootstrap";
+import styles from "./SwapButton.module.css";
+
+interface IProps {
+  connectPending: boolean;
+  swapPending: boolean;
+  approvePending: boolean;
+  hasEnoughFund: boolean;
+}
+
+const SwapButton: React.FC<IProps> = ({
+  connectPending,
+  swapPending,
+  approvePending,
+  hasEnoughFund,
+}) => {
+  const { address } = useSelector(selectUser);
+  const { swapDirection, value, tokenType, approved } = useSelector(selectSwap);
+
+  return (
+    <Button
+      variant="primary"
+      type="submit"
+      disabled={
+        connectPending ||
+        swapPending ||
+        approvePending ||
+        (address !== "" && !hasEnoughFund)
+      }
+      className={styles.btn}
+    >
+      {connectPending || swapPending || approvePending ? (
+        <Spinner
+          as="span"
+          animation="border"
+          role="status"
+          aria-hidden="true"
+        />
+      ) : address ? (
+        value === 0 ? (
+          "Enter an Amount"
+        ) : hasEnoughFund ? (
+          approved || swapDirection === "BuyToken" ? (
+            "Swap"
+          ) : (
+            "Approve token"
+          )
+        ) : tokenType ? (
+          "Insufficient Balance"
+        ) : (
+          "Select a Token"
+        )
+      ) : (
+        "Connet to a Wallet"
+      )}
+    </Button>
+  );
+};
+
+export default SwapButton;
