80d1a3b1ab75309ca3c52f85383ed27037a60032
REGO350 <riokanehiro228pa@gmail.com>
2021-08-20 10:51:48 +0900

    minor

--- src/hooks/usePrevious.ts
+++ src/hooks/usePrevious.ts
@@ -1,10 +1,9 @@
 import { useEffect, useRef } from "react";
 
-export const usePrevious = <T>(value: T) => {
+export const usePrevious = <T>(value: T): T => {
   const ref = useRef<T>(value);
   useEffect(() => {
     ref.current = value;
-  });
+  }, [value]);
   return ref.current;
 };
-
