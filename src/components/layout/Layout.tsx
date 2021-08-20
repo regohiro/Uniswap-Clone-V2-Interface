1a4378b6a1c8d81f10419da2cf8c8aaeb6874901
REGO350 <riokanehiro228pa@gmail.com>
2021-08-20 14:38:40 +0900

    [add] event listener empty component

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import EventListener from '../EventListener'
 import MainNavigation from './MainNavigation'
 
 const Layout: React.FC = ({children}) => {
@@ -6,6 +7,7 @@ const Layout: React.FC = ({children}) => {
     <>
       <MainNavigation/>
       <main>{children}</main>
+      <EventListener/>
     </>
   )
 }
