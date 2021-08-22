ddaab58942e16d863362c2f8c2bf30621131ef6c
REGO350 <62818999+REGO350@users.noreply.github.com>
2021-08-22 14:48:16 +0900

    Update vercel-ignore-build-step.sh

--- vercel-ignore-build-step.sh
+++ vercel-ignore-build-step.sh
@@ -2,9 +2,9 @@
 
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
-if [[ "$VERCEL_GIT_COMMIT_REF" == "staging" ]] ; then
+if [[ "$VERCEL_GIT_COMMIT_REF" == "staging" || "$VERCEL_GIT_COMMIT_REF" == "main"  ]] ; then
   # Proceed with the build
-  echo "✅ - Build can proceed"
+    echo "✅ - Build can proceed"
   exit 1;
 
 else
